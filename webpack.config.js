df18423cd1bc72eab15092e287ca933000d7c5d1
unknown <yagoubi.aek.2@gmail.com>
2023-09-28 00:55:38 +0100

    config webpack to read jsx files

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+
+module.exports = {
+    entry: "./src/renderer/index.jsx",
+    output: {
+        path: path.resolve(__dirname, "out"), 
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            // loads .js/jsx/json files
+            {
+                test: /\.jsx?$/,
+                include: [path.resolve(__dirname, "src/renderer")],
+                loader: "babel-loader",
+                resolve: {
+                    extensions: [".js", ".jsx", ".json"]
+                }
+            }
+        ]
+    }
+};
