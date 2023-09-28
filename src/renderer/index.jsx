35bb40cb56b4513202201c2fc37f3cbc66f3d79b
unknown <yagoubi.aek.2@gmail.com>
2023-09-28 01:15:38 +0100

    add css and scss loader

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import "./index.scss";
 const container = document.getElementById("target");
 const root = createRoot(container);
 
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,30 @@ module.exports = {
                 use: {
                     loader: "html-loader"
                 }
+            },
+            {
+                // loads .css files
+                test: /\.css$/,
+
+                include: [path.resolve(__dirname, "src/renderer")],
+                use: [
+                    "style-loader",
+                    "css-loader"]
+            }
+
+            ,
+            //scss
+            {
+                test: /\.s[ac]ss$/i,
+                include: [path.resolve(__dirname, "src/renderer")],
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
             }
         ]
     },
